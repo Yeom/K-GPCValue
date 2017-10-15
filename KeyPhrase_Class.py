00061f74cc9b7b0c05c4a1a8a201a6cfcb545b7c
Hongseon, Yeom <yeomjohn@gmail.com>
2017-10-15 17:30:59 +0900

    Combined Module version

--- KeyPhrase_Class.py
+++ KeyPhrase_Class.py
@@ -26,52 +26,71 @@ def preprocess_document(document):
         result += word+'_'+tag+' '
     return result.strip()
 class Keyphrase():
-    def __init__(self,pwd,window_size = 10):
-        with codecs.open(pwd,'r',encoding='utf8') as doc:
-            self.Final_Info = {}
-            self.cValue_Info = {}
-            self.key_path = pwd.split('/')[-1].split('.')[0]
-            self.word_Dic = {}
-            self.document = []
-            self.posTags = []
-            self.position = {}
-            self.upperDic = {}
-            doc_idx = 0
-            self.doc_Lines = preprocess_document(doc.read())
-            self.doc_Line = self.doc_Lines
-            tokens = self.doc_Lines.split()
-            self.doc_len = len(tokens)
-            for token in tokens:
-                word = token.split('_')[0]
-                try : pos = token.split('_')[1]
-                except : print word
-                for i in xrange(len(word)):
-                    if word[i].isupper():
-                        self.upperDic[word.lower()] = self.upperDic.get(word.lower(), 0.0) + 1.0
-                        break
-                word = word.strip().lower()
-                self.word_Dic[word] = self.word_Dic.get(word,0) + 1.0
-                self.document.append(word)
-                self.posTags.append(pos)
+    def __init__(self):
+        pass 
+    def __call__(self, *args):
+        call_flag = False
+        pwd = ''
+        window_size = 0
+        sentence_ = ''
+
+        if 'pwd' in args[0].keys():
+            pwd = args[0]['pwd']
+            call_flag = True
+        if 'window_size' in args[0].keys():
+            window_size = args[0]['window_size']
+        if 'sentence' in args[0].keys():
+            sentence_ = args[0]['sentence']
+
+        self.Final_Info = {}
+        self.cValue_Info = {}
+        self.word_Dic = {}
+        self.document = []
+        self.posTags = []
+        self.position = {}
+        self.upperDic = {}
+        doc_idx = 0
+
+        if call_flag:
+            with codecs.open(pwd,'r',encoding='utf8') as doc:
+                self.doc_Lines = preprocess_document(doc.read())
+        else:
+            self.doc_Lines = preprocess_document(sentence_)
+
+        self.doc_Line = self.doc_Lines
+        tokens = self.doc_Lines.split()
+        self.doc_len = len(tokens)
+        for token in tokens:
+            word = token.split('_')[0]
+            try : pos = token.split('_')[1]
+            except : continue
+            for i in xrange(len(word)):
+                if word[i].isupper():
+                    self.upperDic[word.lower()] = self.upperDic.get(word.lower(), 0.0) + 1.0
+                    break
+            word = word.strip().lower()
+            self.word_Dic[word] = self.word_Dic.get(word,0) + 1.0
+            self.document.append(word)
+            self.posTags.append(pos)
                         
-                flag1 = KF.isGoodPOS(pos)
-                flag2 = KF.isSpecialToken(word)
-                if(flag1 == False or flag2 == False):
-                    doc_idx = doc_idx +1
-                    continue
-                doc_idx = doc_idx + 1
-                if self.position.get(word) == None :
-                    position_list = []
-                    self.position[word] = position_list
+            flag1 = KF.isGoodPOS(pos)
+            flag2 = KF.isSpecialToken(word)
+            if(flag1 == False or flag2 == False):
+                doc_idx = doc_idx +1
+                continue
+            doc_idx = doc_idx + 1
+            if self.position.get(word) == None :
+                position_list = []
+                self.position[word] = position_list
                     
-                self.position[word].append(doc_idx)
+            self.position[word].append(doc_idx)
         p = re.compile('_[A-Z\.\!\@\#\$\%\^\&\*\?]+')
         dot = re.compile('[\.\!\@\#\$\%\^\&\*\?]')
         self.doc_Lines = p.sub('',self.doc_Lines)
         self.doc_Lines = dot.sub('',self.doc_Lines)
         self.doc_Lines = self.doc_Lines.lower()
 
-    def Basic_GraphModel(self,window_size,file_name):
+    def Basic_GraphModel(self,window_size):
         self.srGraph,self.new_srGraph = KF.buildGraph(self.document, self.position,int(window_size), self.posTags) # Building Document Graph
         self.srGraph = KF.WeightGraph(self.word_Dic, self.srGraph, self.new_srGraph)#Adjust weighting scheme to graph edge
         self.srScore, self.srScore_avg = KF.BasicRankAlgorithm(self.srGraph, self.position) # Ranking Vertex Scores
@@ -105,24 +124,41 @@ class FileOperations:
 
 ###################################################################################
 def main(args):
-    if True:
-        with open(args.data) as f_:
-            for line in f_:
-                data_path = line.strip()
-                fileList = f.GetFiles(data_path)
-                fileHandler = FileOperations()
-                fileHandler.open_file_combined()
-    
-                for idx in xrange(len(fileList)):
-                    file_name = fileList[idx].split('/')[-1].split('.')[0]
-                    AKE = Keyphrase(fileList[idx], args.window)
-                    AKE.Basic_GraphModel(args.window,file_name)
-                    AKE.CValueModel()
-                    KeyPhrase = AKE.FinalKeyPhrase
-                    fileHandler.write_to_file_combined(file_name, KeyPhrase, int(args.key))
-                fileHandler.close_file_combined()
+    _module_ = Keyphrase()
+    with open(args.data) as f_:
+        for line in f_:
+            data_path = line.strip()
+            fileList = f.GetFiles(data_path)
+            fileHandler = FileOperations()
+            fileHandler.open_file_combined()
+            for idx in xrange(len(fileList)):
+                file_name = fileList[idx].split('/')[-1].split('.')[0]
+                param = {}
+                param['pwd'] = fileList[idx]
+                param['window_size'] = args.window
+                _module_(param)
+                _module_.Basic_GraphModel(args.window)
+                _module_.CValueModel()
+                KeyPhrase = _module_.FinalKeyPhrase
+                fileHandler.write_to_file_combined(file_name, KeyPhrase, int(args.key))
+            fileHandler.close_file_combined()
 #Run the model : python KeyPhrase_Class.py --data filepath.txt --key #(default 15) --window #(default 10)
-
+def module(_module_,sentence, window_size=5, key=5):
+    fileHandler = FileOperations()
+    fileHandler.open_file_combined()
+    param = {}
+    Keyword = []
+    param['sentence'] = sentence
+    param['window_size'] = window_size
+    _module_(param)
+    _module_.Basic_GraphModel(window_size)
+    _module_.CValueModel()
+    KeyPhrase = _module_.FinalKeyPhrase
+    
+    for idx in xrange(key):
+        key , val = KeyPhrase[idx]
+        Keyword.append(key)
+    return Keyword
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--data",help="data path file")
---
+++ test.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import KeyPhrase_Class as KC
+_module_ = KC.Keyphrase()
+_ = KC.module(_module_,'현행 형사소송법 체계에 따라 검찰과 마찬가지로 기소법정주의는 채택하지 않는다. 기소법정주의는 일정한 전제조건을 정해두고 조건이 충족되면 반드시 기소해야 하는 원칙을 말한다. 우리 형사소송법은 검사에게 기소·불기소 재량의 여지를 인정하는 기소편의주의를 채택하고 있다.',5,5)
+print _
